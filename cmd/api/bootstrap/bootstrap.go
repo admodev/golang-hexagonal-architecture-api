a3101c3ab9dc12ccf838cb223f62fb14b5101f8b
admodev <admodevcodes@outlook.com>
2022-04-11 23:36:25 -0300

    add dotenv for sensitive data

--- cmd/api/bootstrap/bootstrap.go
+++ cmd/api/bootstrap/bootstrap.go
@@ -6,17 +6,33 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"strconv"
 )
 
-const (
-	host   = "localhost"
-	port   = 8080
-	dbUser = "root"
-	dbPass = "admocode"
-	dbHost = "localhost"
-	dbPort = "3306"
-	dbName = "bctec"
-)
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+// TODO: Move this to separate file...
+var host = goDotEnvVariable("HOST")
+var portNum = goDotEnvVariable("PORT")
+var dbUser = goDotEnvVariable("DB_USER")
+var dbPass = goDotEnvVariable("DB_PASS")
+var dbHost = goDotEnvVariable("DB_HOST")
+var dbPort = goDotEnvVariable("DB_PORT")
+var dbName = goDotEnvVariable("DB_NAME")
+var port, _ = strconv.ParseUint(portNum, 10, 64)
 
 func Run() error {
 	mysqlURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
@@ -27,7 +43,7 @@ func Run() error {
 	}
 
 	reportRepository := mysql.NewReportRepository(db)
-	srv := server.New(host, port, reportRepository)
+	srv := server.New(host, uint(port), reportRepository)
 
 	return srv.Run()
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/huandu/go-sqlbuilder v1.13.0
+	github.com/joho/godotenv v1.4.0
 )
 
 require (
