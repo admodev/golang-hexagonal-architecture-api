84c5ae81b7567c2cc091cdc32f8e98d1b34d0999
admodev <admodevcodes@outlook.com>
2022-04-13 12:58:22 -0300

    add JWT creation handler func

--- internal/platform/server/handler/users/create.go
+++ internal/platform/server/handler/users/create.go
@@ -2,9 +2,17 @@ package users
 
 import (
 	users "bctec/internal"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"net/http"
 )
+
+type JWTClaims struct {
+	Email string `json:"email"`
+	Role  string `json:"role"`
+	jwt.StandardClaims
+}
 
 type createRequest struct {
 	Username  string `json:"username" binding:"required"`
@@ -14,6 +22,28 @@ type createRequest struct {
 	Website   string `json:"website"`
 	Password  string `json:"password" binding:"required"`
 	Role      string `json:"role" binding:"required"`
+}
+
+func CreateJWT(email, role string) string {
+	claims := JWTClaims{
+		email,
+		role,
+		jwt.StandardClaims{
+			ExpiresAt: 15000,
+			Issuer:    "BCTEC",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	secret := "secretaca"
+	signedJwtoken, err := token.SignedString(secret)
+
+	if err != nil {
+		jwtError := fmt.Sprintf("Error signing JSONWebToken! %v", err.Error())
+
+		return jwtError
+	}
+
+	return signedJwtoken
 }
 
 func CreateHandler(usersRepository users.UserRepository) gin.HandlerFunc {
